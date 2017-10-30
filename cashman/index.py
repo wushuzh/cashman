57c2d81533c054769f99d3082cb8414eb119527f
wushuzh <zhangjiaqiang@gmail.com>
2017-10-30 15:44:30 +0800

    add end points for incomes get and post

--- cashman/index.py
+++ cashman/index.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+incomes = [
+    {'description': 'salary', 'amount': 5000}
+]
 
-@app.route("/")
-def hello_restful():
-    return "Hello RESTful!"
+
+@app.route('/incomes')
+def get_incomes():
+    return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    incomes.append(request.get_json())
+    return '', 204
